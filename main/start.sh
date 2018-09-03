156607c482e6180d204ce5bf3db34d06af602cd4
Mark <drkmsmithjr@gmail.com>
2018-09-03 15:56:10 -0700

    deleted:    src/main.py 	deleted:    src/process.py 	modified:   start.sh

--- main/start.sh
+++ main/start.sh
@@ -2,7 +2,7 @@
 
 #start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-./resin-wifi-connect --clear=false
+./wifi-connect --clear=false
 
 # Default to UTC if no TIMEZONE env variable is set
 echo "Setting time zone to ${TIMEZONE=UTC}"
