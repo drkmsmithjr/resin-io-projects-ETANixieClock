e508ae8acf0cfecd2a177a22e4e62d8eeaf05211
Mark <drkmsmithjr@gmail.com>
2018-09-04 19:50:19 -0700

    Added all the options back into start.sh file.  removed the DONE command at end

--- main/start.sh
+++ main/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 #start wifi-connect.  This will start an access point if none is found
-#export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-#./wifi-connect --portal-ssid="ETANixieClock Connect" --activity-timeout=20
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+./wifi-connect --portal-ssid="ETANixieClock Connect" --activity-timeout=20
 
 # Default to UTC if no TIMEZONE env variable is set
 echo "Setting time zone to ${TIMEZONE=America/Los_Angeles}"
@@ -11,6 +11,6 @@ echo "${TIMEZONE}" > /etc/timezone
 dpkg-reconfigure tzdata
 
 # GoogleKey will be passed from Resin
-#python /app/ETAclock.py ${GOOGLEKEY}
+python /app/ETAclock.py ${GOOGLEKEY}
 
 
