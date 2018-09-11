af5cfba2ac8fa4f302f354796fe4183ec8eea7c8
Mark <drkmsmithjr@gmail.com>
2018-09-11 00:13:48 -0700

    Not sure about times.

--- main/ETAclock.py
+++ main/ETAclock.py
@@ -348,7 +348,7 @@ print("after timer thread call")
 # Burnin Times
 BurnInMinutes = 20
 BurnInStart = 20
-BurnInStop = 22
+BurnInStop = 2
 DigitsToTest = [0,3,4,9,1,2,5,6,7,8]
 DigitsTimeTest = [1,.5,.5,.5,.1,.1,.1,.1,.1,.1]
 DigIndex = 0
